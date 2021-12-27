4136ad4a05a9cb93c1bc485fff6a7fa96efef81a
Israk Kayum <israk.kayum@gmail.com>
2021-12-27 12:08:40 +0600

    first

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5900;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.60ufn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('ImageData');
+        const imageCollection = database.collection('image');
+
+        app.post('/image', async(req, res) => {
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const image = {image: imageBuffer}
+            const result = await imageCollection.insertOne(image);
+            res.json(result);
+        });
+
+        app.get('/image', async(req, res) => {
+            const cursor = imageCollection.find({});
+            const image = await cursor.toArray();
+            res.json(image);
+        })
+    }
+
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Ema jon server is running and running');
+});
+
+app.listen(port, () => {
+    console.log('Server running at port', port);
+});
